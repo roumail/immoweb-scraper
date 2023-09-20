5942d5833b216f1d67640c3e964987595e267fc2
Rohail Taimour <rohail.taimour@gmail.com>
2023-09-20 08:02:52 +0200

    fix the clicking on button

--- immoweb_scraper/setup.py
+++ immoweb_scraper/setup.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Chrome as WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
 def browser_setup():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     browser = webdriver.Chrome(options=options)
     return browser
 
@@ -19,9 +19,16 @@ def page_setup(browser: WebDriver, webpage_url: str):
     WebDriverWait(browser, 4)
 
     # wait and click privacy.. only the first time browser page opened
-    # every other time, we skip this part
-    try:
-        browser.find_element(By.XPATH, "//button[@id='uc-btn-accept-banner']").click()
-    except NoSuchElementException:
-        pass
+    # the button itself has the id "//button[@data-testid='uc-accept-all-button']"
+    ok_button = browser.execute_script(
+        'return document.querySelector("#usercentrics-root").shadowRoot.querySelector("#uc-center-container > div.sc-eBMEME.ixkACg > div > div.sc-jsJBEP.bHqEwZ > div > div > button.sc-dcJsrY.gwuZOI");'
+    )
+    if ok_button:
+        ok_button.click()
+
+    # Wait until the overflow box is hidden
+    wait = WebDriverWait(browser, 10)
+    wait.until_not(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#usercentrics-root"))
+    )
     return
--- immoweb_scraper/url.py
+++ immoweb_scraper/url.py
@@ -1,2 +1,32 @@
-def build_url():
-    return "https://www.immoweb.be/en/search/apartment/for-rent?countries=BE&hasKitchenSetup=true&isAnInvestmentProperty=false&isFurnished=false&maxBedroomCount=4&maxPrice=1400&minBedroomCount=2&minSurface=90&postalCodes=1030,1040,1050,1060,1150,1180,1190,1200&priceType=MONTHLY_RENTAL_PRICE&propertySubtypes=APARTMENT,DUPLEX,PENTHOUSE,TRIPLEX,LOFT&provinces=BRUSSELS&page={}"
+from urllib.parse import urlencode
+
+
+class ImmoWebURLBuilder:
+    def __init__(self):
+        self.base_url = "https://www.immoweb.be/en/search/apartment/{}?"
+        self.default_params = {
+            "countries": "BE",
+            "hasKitchenSetup": "true",
+            "isAnInvestmentProperty": "false",
+            "isFurnished": "false",
+            "maxBedroomCount": "4",
+            "minBedroomCount": "2",
+            "minSurface": "90",
+            "postalCodes": "1030,1040,1050,1060,1150,1180,1190,1200",
+            "provinces": "BRUSSELS",
+            "page": "1",
+        }
+
+    def for_rent(self, **kwargs):
+        # Set specific parameters for rental
+        rent_params = {"priceType": "MONTHLY_RENTAL_PRICE", "maxPrice": "1400"}
+        # Merge default parameters with rental-specific ones
+        params = {**self.default_params, **rent_params, **kwargs}
+        return self.base_url.format("for-rent") + urlencode(params)
+
+    def for_sale(self, **kwargs):
+        # Set specific parameters for sale
+        sale_params = {"priceType": "SALE_PRICE"}
+        # Merge default parameters with sale-specific ones
+        params = {**self.default_params, **sale_params, **kwargs}
+        return self.base_url.format("for-sale") + urlencode(params)
