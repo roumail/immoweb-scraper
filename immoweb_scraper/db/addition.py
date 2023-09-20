0d42f9d6212b223c9396b6db05f7f3a8a984e6d6
Rohail Taimour <rohail.taimour@gmail.com>
2023-09-20 18:13:07 +0200

    add only new properties

--- immoweb_scraper/db/addition.py
+++ immoweb_scraper/db/addition.py
@@ -1,4 +1,5 @@
 import typing as tp
+from collections import Counter
 
 from loguru import logger
 
@@ -14,10 +15,43 @@ def insert_properties(
     properties: list[tp.Union["PurchaseProperty", "RentalProperty"]],
     table_class: tp.Type[Base],
 ):
+    # 1. Verify that we only have unique identifiers in the properties list
+    identifiers_list = [item.immoweb_identifier for item in properties]
+    identifier_counts = Counter(identifiers_list)
+    duplicates_in_list = {
+        identifier: count
+        for identifier, count in identifier_counts.items()
+        if count > 1
+    }
+    assert not duplicates_in_list, f"Duplicates found in the list: {duplicates_in_list}"
+
+    # 2. Check against the database to ensure you're not inserting properties that already exist
+    if table_class == RentalPropertyTable:
+        existing_properties = get_all_rental_properties(db_conn)
+    else:
+        existing_properties = get_all_purchase_properties(db_conn)
+
+    existing_identifiers = set(prop.immoweb_identifier for prop in existing_properties)
+    new_properties = [
+        prop
+        for prop in properties
+        if prop.immoweb_identifier not in existing_identifiers
+    ]
+
     with db_conn.session_scope() as session:
-        for prop in properties:
+        for prop in new_properties:
             db_property = table_class(**prop.model_dump())
             session.add(db_property)
+
+
+def get_all_rental_properties(db_conn: "DBConnection"):
+    with db_conn.session_scope() as session:
+        return session.query(RentalPropertyTable).all()
+
+
+def get_all_purchase_properties(db_conn: "DBConnection"):
+    with db_conn.session_scope() as session:
+        return session.query(PurchasePropertyTable).all()
 
 
 def add_properties(
